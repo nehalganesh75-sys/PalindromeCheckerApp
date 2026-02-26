881b7989cdc957e10b90f07486d3acc454bbad01
nehalganesh75 <nehalganesh75@gmail.com>
2026-02-26 10:39:12 +0530

    UC9:Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,89 +1,32 @@
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class PalindromeCheckerApp {
 
-    // Convert string to linked list
-    public static Node createList(String str) {
-        if (str == null || str.length() == 0) return null;
+    // Recursive method to check palindrome
+    public static boolean isPalindrome(String str, int start, int end) {
 
-        Node head = new Node(str.charAt(0));
-        Node current = head;
-
-        for (int i = 1; i < str.length(); i++) {
-            current.next = new Node(str.charAt(i));
-            current = current.next;
+        // Base Condition: If pointers cross or meet
+        if (start >= end) {
+            return true;
         }
 
-        return head;
+        // If characters don't match
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call for next inner characters
+        return isPalindrome(str, start + 1, end - 1);
     }
 
-    // Reverse linked list
-    public static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-        Node next = null;
-
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        return prev;
-    }
-
-    // Check palindrome
-    public static boolean isPalindrome(Node head) {
-        if (head == null || head.next == null) return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        // Find middle using fast & slow pointer
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // If odd length, move slow one step ahead
-        if (fast != null) {
-            slow = slow.next;
-        }
-
-        // Reverse second half
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        // Compare both halves
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-
-    // Main method
     public static void main(String[] args) {
-        String str = "madam";
-        Node head = createList(str);
 
-        if (isPalindrome(head)) {
-            System.out.println(str + " is a Palindrome");
+        String input = "racecar";
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        if (result) {
+            System.out.println(input + " is a Palindrome");
         } else {
-            System.out.println(str + " is NOT a Palindrome");
+            System.out.println(input + " is NOT a Palindrome");
         }
     }
 }
